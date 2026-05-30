43357828dca3fd3e8b556289b5a6318bd1f4a882
Sailen Dutta <sailen.muggle@gmail.com>
2026-05-30 19:29:56 +0530

    Add gaussian and sine initial conditions

--- include/InitialConditions.h
+++ include/InitialConditions.h
@@ -1,7 +1,13 @@
 #pragma once
 #include "Field1D.h"
+#include <numbers>
+#include <cmath>
 
 namespace InitialConditions{
 
     void squarePulse(Field1D& field, double x_start, double x_end, double value);
+
+    void gaussian(Field1D& field, double center, double sigma);
+
+    void sinewave(Field1D& field);
 }
--- src/InitialConditions.cpp
+++ src/InitialConditions.cpp
@@ -1,10 +1,27 @@
 #include "InitialConditions.h"
 
+
 void InitialConditions::squarePulse(Field1D& field, double x_start, double x_end, double value){
     for (size_t i = 0; i < field.size(); ++i){
         double x = field.grid().x(i);
         if (x >= x_start && x <= x_end){
             field[i] = value;
         }
+    }
+}
+
+void InitialConditions::gaussian(Field1D& field, double center, double sigma){
+    for (size_t i = 0; i < field.size(); ++i){
+        double x = field.grid().x(i);
+        double exponent = -((x - center) * (x - center))/(2.0 * sigma * sigma);
+        field[i] = std::exp(exponent);
+    }
+}
+
+void InitialConditions::sinewave(Field1D& field){
+    constexpr double pi = std::numbers::pi;
+    for (size_t i = 0; i < field.size(); ++i){
+        double x = field.grid().x(i);
+        field[i] = std::sin(2 * pi * x);
     }
 }
