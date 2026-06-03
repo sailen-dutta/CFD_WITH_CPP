8c2d2d5dcc704eeb9609dcdeae86164bb53bceba
Sailen Dutta <sailen.muggle@gmail.com>
2026-06-03 21:21:00 +0530

    Add square pulse exact solution

--- include/physics/ExactSolutions.h
+++ include/physics/ExactSolutions.h
@@ -3,4 +3,6 @@
 
 namespace ExactSolutions{
     void sineWave(Field1D& u_exact, double c, double t);
+
+    void squarePulse(Field1D& u_exact, double c, double t, double pulse_start, double pulse_end, double value);
 }
--- src/physics/ExactSolutions.cpp
+++ src/physics/ExactSolutions.cpp
@@ -1,5 +1,6 @@
 #include "physics/ExactSolutions.h"
 #include <cmath>
+#include <numbers>
 
 namespace ExactSolutions {
     void sineWave(Field1D& u_exact, double c, double t){
@@ -7,6 +8,30 @@ namespace ExactSolutions {
         for (size_t i = 0; i < u_exact.size(); i++){
             double x = u_exact.grid().x(i);
             u_exact[i] = std::sin(4 * pi * (x - c * t));
+        }
+    }
+
+    void squarePulse(Field1D& u_exact, double c, double t, double pulse_start, double pulse_end, double value){
+        u_exact.fill(0.0);
+        for (size_t i = 0; i < u_exact.size(); ++i){
+            double x = u_exact.grid().x(i);
+
+            /* Shift coordinates backward */
+            double x_shift = x - c * t;
+
+            /* Periodic wrap */
+            while (x_shift < 0.0){
+                x_shift += 1.0;
+            }
+
+            while (x_shift > 1.0){
+                x_shift -= 1.0;
+            }
+
+            /* Inside pulse? */
+            if (x_shift >= pulse_start && x_shift <= pulse_end){
+                u_exact[i] = value;
+            }
         }
     }
 }
