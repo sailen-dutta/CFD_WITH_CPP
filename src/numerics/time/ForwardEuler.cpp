efbca304dc6f6e7856503a2fe8353ec57b97f13f
Sailen Dutta <sailen.muggle@gmail.com>
2026-06-18 20:02:27 +0530

    Forward Euler time integrator

---
+++ src/numerics/time/ForwardEuler.cpp
@@ -0,0 +1,10 @@
+#include "numerics/time/ForwardEuler.h"
+
+void ForwardEuler :: advance(Field1D& u, const FiniteVolumeOperator& op, double dt) const {
+    Field1D rhs(u.grid());
+    op.computeRHS(u, rhs);
+
+    for (size_t i = 0; i < u.size(); ++i){
+        u[i] += dt * rhs[i];
+    }
+}
