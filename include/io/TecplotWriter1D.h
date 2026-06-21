468ca34b2b3f840c12abef9e46083825bc3aab10
Sailen Dutta <sailen.muggle@gmail.com>
2026-06-21 14:18:55 +0530

    Added Tecplot writer class

---
+++ include/io/TecplotWriter1D.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "core/Field1D.h"
+#include "io/OutputWriter.h"
+#include <string>
+
+class TecplotWriter1D : public OutputWriter {
+    public:
+        void write(const Field1D& field, const std::string& filename) override;
+        std::string extension() const override;
+};
