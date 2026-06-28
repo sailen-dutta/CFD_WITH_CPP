82472732533a8c860574181d8757e2a55dd753a8
Sailen Dutta <sailen.muggle@gmail.com>
2026-06-28 19:51:47 +0530

    Eliminated multiple if-else statements

--- src/numerics/time/TimeIntegratorFactory.cpp
+++ src/numerics/time/TimeIntegratorFactory.cpp
@@ -3,18 +3,38 @@
 #include "numerics/time/RK2.h"
 #include "numerics/time/SSPRK3.h"
 #include <stdexcept>
+#include <unordered_map>
+#include <memory>
+#include <string>
 
-std::unique_ptr<TimeIntegrator> TimeIntegratorFactory::create(const std::string& integrator_type){
-    if (integrator_type == "forward_euler"){
+namespace {
+    std::unique_ptr<TimeIntegrator> createForwardEuler(){
         return std::make_unique<ForwardEuler>();
     }
-    else if (integrator_type == "rk2"){
+
+    std::unique_ptr<TimeIntegrator> createRK2() {
         return std::make_unique<RK2>();
     }
-    else if (integrator_type == "ssprk3"){
+
+    std::unique_ptr<TimeIntegrator> createSSPRK3() {
         return std::make_unique<SSPRK3>();
     }
-    else{
-        throw std::runtime_error("Unknown time integrator: " + integrator_type);
+
+    using Creator = std::unique_ptr<TimeIntegrator> (*)();
+
+    std::unordered_map<std::string, Creator> lookup_table = {
+        {"forward_euler", createForwardEuler},
+        {"rk2",           createRK2},
+        {"ssprk3",        createSSPRK3}
+    };
+}
+
+std::unique_ptr<TimeIntegrator> TimeIntegratorFactory::create(const std::string& integrator_type){
+    auto it = lookup_table.find(integrator_type);
+
+    if (it == lookup_table.end()){
+        throw std::runtime_error("Unknown time integrator type: " + integrator_type);
     }
+
+    return it->second();
 }
