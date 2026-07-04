99b78d21cd28e95a7114c5c4d6b27f3c32d12c09
Sailen Dutta <sailen.muggle@gmail.com>
2026-07-04 12:30:06 +0530

    Modified first order fv operator for reconstruction

--- src/numerics/spatial/FirstOrderFVOperator.cpp
+++ src/numerics/spatial/FirstOrderFVOperator.cpp
@@ -1,16 +1,29 @@
 #include "numerics/spatial/FirstOrderFVOperator.h"
 
-FirstOrderFVOperator::FirstOrderFVOperator(const NumericalFlux& flux) : flux_(flux) {}
+FirstOrderFVOperator::FirstOrderFVOperator(const NumericalFlux& flux, const Reconstruction& reconstruction) : flux_(flux), reconstruction_(reconstruction) {}
 
 void FirstOrderFVOperator::computeRHS(const Field1D& u, Field1D& rhs) const {
     double dx = u.grid().dx();
     size_t N = u.size();
 
     for (size_t i = 0; i < N; ++i){
-        size_t im1 = (i == 0)? N - 1 : i - 1;
-        size_t ip1 = (i + 1) % N;
-        double F_right = flux_.compute(u[i], u[ip1]);
-        double F_left = flux_.compute(u[im1], u[i]);
-        rhs[i] = -(F_right - F_left) / dx; 
+        /*uL_left  = value approaching the left interface from the left
+          uL_right = value approaching the right interface from the left
+          uR_left  = value approaching the left interface from the right
+          uR_right = value approaching the right interface from the right*/
+        double uL_left, uL_right, uR_left, uR_right;
+
+        /* Interface i+1/2 */
+        reconstruction_.reconstruct(u,i,uL_right,uR_right);
+
+        size_t left_interface = (i == 0) ? N - 1 : i - 1;
+        
+        /* Interface i-1/2 */
+        reconstruction_.reconstruct(u,left_interface,uL_left, uR_left);
+
+        double F_right = flux_.compute(uL_right, uR_right);
+        double F_left = flux_.compute(uL_left, uR_left);
+
+        rhs[i] = -(F_right - F_left) / dx;
     }
 }
